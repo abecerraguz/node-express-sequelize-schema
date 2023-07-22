ea59cdbb9e1e5758b22548e58f678866e703fca0
Alejandro Mauricio Becerra Guzman <abecerraguz@MacBook-Pro-de-Alejandro.local>
2023-07-21 22:15:24 -0400

    Se guardan los cambios

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,19 @@
 import { Sequelize } from 'sequelize';
 
 // DB LOCAL
-// const db = new Sequelize('Clinica_Postgres', 'postgres', 'postgres', {
-//     host:'localhost',
-//     dialect:'postgres',
-//     logging:true
-// });
+const db = new Sequelize( process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+    host:process.env.PGHOST,
+    dialect:'postgres',
+    logging:true
+});
+
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//     host: 'localhost',
+//     dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+//   });
 
 // DB PRODUCCION
-const db = new Sequelize( process.env.DATABASE_URL )
+// const db = new Sequelize( process.env.DATABASE_URL )
 
 export default db
 
